67a4e48816ab0c4f1dc02796d49bc808751f9a97
deechael <DeeChael@outlook.com>
2025-05-16 21:45:56 +0800

    feat: re-added v1 extensions

--- core/src/main/kotlin/ink/pmc/advkt/v2/extension/ComponentExtension.kt
+++ core/src/main/kotlin/ink/pmc/advkt/v2/extension/ComponentExtension.kt
@@ -1,6 +1,8 @@
 package ink.pmc.advkt.v2.extension
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.TextReplacementConfig
+import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.serializer.ansi.ANSIComponentSerializer
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
@@ -28,4 +30,50 @@ fun Component.plain(): String {
 
 fun Component.ansi(): String {
     return ANSIComponentSerializer.ansi().serialize(this)
+}
+
+fun Component.replace(string: String, text: String, literal: Boolean = false): Component {
+    return this.replace(string, Component.text(text), literal)
+}
+
+fun Component.replace(string: String, component: Component, literal: Boolean = false): Component {
+    val replaceConfig = TextReplacementConfig.builder()
+        .apply {
+            if (literal)
+                this.matchLiteral(string)
+            else
+                this.match(string)
+        }
+        .replacement(component)
+        .build()
+
+    return this.replaceText(replaceConfig)
+}
+
+fun Component.replace(string: String, literal: Boolean = false, replacement: (Component) -> Component): Component {
+    val replaceConfig = TextReplacementConfig.builder()
+        .apply {
+            if (literal)
+                this.matchLiteral(string)
+            else
+                this.match(string)
+        }
+        .replacement { builder ->
+            return@replacement replacement(builder.build())
+        }
+        .build()
+
+    return this.replaceText(replaceConfig)
+}
+
+fun Component.replaceColor(targetColor: TextColor, newColor: TextColor): Component {
+    val updatedComponent = if (this.color() == targetColor) {
+        this.color(newColor)
+    } else {
+        this
+    }
+
+    return updatedComponent.children().fold(updatedComponent.children(emptyList())) { component, child ->
+        component.append(child.replaceColor(targetColor, newColor))
+    }
 }
