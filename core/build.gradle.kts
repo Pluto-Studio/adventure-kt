aba743d24e706ad505cd21c87407793be060601a
nostalfinals <yuu8583@proton.me>
2025-05-17 12:15:27 +0800

    build: configure maven publish

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,3 +1,27 @@
+plugins {
+    id("maven-publish")
+}
+
 kotlin {
     jvmToolchain(8)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "nostal"
+            url = uri(
+                if (version.toString().endsWith("SNAPSHOT")) {
+                    "https://maven.nostal.ink/repository/maven-snapshots/"
+                } else {
+                    "https://maven.nostal.ink/repository/maven-releases/"
+                }
+            )
+            credentials(PasswordCredentials::class)
+        }
+    }
+
+    publications.create<MavenPublication>("maven") {
+        artifact(tasks.jar)
+    }
 }
